35b2886c26f47a642c10679ac08e7ab053e0c213
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-08-21 02:03:55 +0700

    init proj

---
+++ crawlerlink/src/parse.rs
@@ -0,0 +1,18 @@
+use std::string::String;
+
+use html5ever::tendril::TendrilSink;
+use html5ever::parse_document;
+use html5ever::rcdom::{RcDom, NodeData, Handle};
+use html5ever::interface::Attribute;
+
+
+pub fn parse_html(source: &str) -> RcDom {
+    parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(&mut source.as_bytes())
+        .unwrap()
+}
+
+pub fn get_url(handle: Handle) -> Vec<String> {
+    let mut urls = vec![];
+}
