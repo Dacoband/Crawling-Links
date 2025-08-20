ca3cf1cddbbb8ed72a1c2b9219283096f2292e72
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-08-21 02:17:58 +0700

    Add HTML parsing functionality and update dependencies, get URLs

--- crawlerlink/src/main.rs
+++ crawlerlink/src/main.rs
@@ -1,3 +1,10 @@
+extern crate html5ever;
+extern crate hyper;
+extern crate url;
+
+
+mod parse;
+
 fn main() {
     println!("Hello, world!");
 }
--- crawlerlink/src/parse.rs
+++ crawlerlink/src/parse.rs
@@ -4,6 +4,7 @@ use html5ever::tendril::TendrilSink;
 use html5ever::parse_document;
 use html5ever::rcdom::{RcDom, NodeData, Handle};
 use html5ever::interface::Attribute;
+use hyper::header::AccessControlRequestHeaders;
 
 
 pub fn parse_html(source: &str) -> RcDom {
@@ -15,4 +16,44 @@ pub fn parse_html(source: &str) -> RcDom {
 
 pub fn get_url(handle: Handle) -> Vec<String> {
     let mut urls = vec![];
+    let mut anchor_tags = vec![];
+
+    get_elements_by_name(handle, "a", &mut anchor_tags);
+
+    for node in anchor_tags{
+        if let NodeData::Element { ref attrs, ..} = node {
+            for attr in attrs.borrow().iter()  {
+                let  Attribute {
+                    ref name,
+                    ref value,
+                } = *attr;
+                if &*(name.local) == "href" {
+                    urls.push(value.to_string());
+                }
+            }
+        }
+    }
+    urls
+}
+
+fn get_elements_by_name(handle: Handle, element_name: &str, out: &mut Vec<NodeData>){
+    let node = handle;
+    if let NodeData::Element {
+        ref  name,
+        ref attrs,
+        ref template_contents,
+        ..
+    } = node.data {
+        if &*(name.local) == element_name {
+            out.push(NodeData::Element { 
+                name: name.clone(),
+                attrs: attrs.clone(),
+                template_contents: template_contents.clone(),
+                mathml_annotation_xml_integration_point: false, 
+            });
+        }
+    }
+    for n in node.children.borrow().iter()  {
+        get_elements_by_name(n.clone(), element_name, out);
+    }
 }
